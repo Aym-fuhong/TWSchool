808329097f19011481303bc008f4086b4f65d75b
Aym-fuhong <18829287015@163.com>
2017-07-18 23:17:26 +0800

    update section2-prastice2 and finish section2-prastice3

--- workspace/gpst-js-basic-collection-practice/main/section-2/practice-2.js
+++ workspace/gpst-js-basic-collection-practice/main/section-2/practice-2.js
@@ -3,12 +3,17 @@
 module.exports = function countSameElements(collection) {
     let result = [];
     for(let i =0 ; i < collection.length; i++) {
-        let obj = isExist(collection[i], result);
+        let obj = findOne(collection[i], result);
         if(obj){
             obj.count++;
-        } else if(collection[i].includes('-')) {
+        } else if (collection[i].includes('-')) {
           let combNum = collection[i].split('-');
+          let obj = findOne(combNum[0], result);
+          if (obj) {
+              obj.count += parseInt(combNum[1]);
+          } else {
           result.push({key: combNum[0], count: parseInt(combNum[1])});
+          }
         }else {
             result.push({key: collection[i], count: 1});
         }
@@ -16,7 +21,7 @@ module.exports = function countSameElements(collection) {
     return result;
 };
 
-function isExist(element, collection) {
+function findOne(element, collection) {
     for(let j =0; j< collection.length; j++){
         if(element === collection[j].key){
             return collection[j];
--- workspace/gpst-js-basic-collection-practice/main/section-2/practice-3.js
+++ workspace/gpst-js-basic-collection-practice/main/section-2/practice-3.js
@@ -1,5 +1,47 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+    let result = [];
+    for (let i =0 ; i < collection.length; i++) {
+        let obj = findOne(collection[i], result);
+        if (obj) {
+            obj.summary++;
+        } else if (isSymbol(collection[i])) {
+            let symbolStr = isSymbol(collection[i]);
+            let obj = findOne(symbolStr.name, result);
+            if (obj) {
+                obj.summary += symbolStr.summary;
+            } else {
+                result.push({name: symbolStr.name, summary: symbolStr.summary});
+            }
+        }
+        else {
+            result.push({name: collection[i], summary: 1});
+        }
+    }
+    return result;
+};
+
+function findOne(element, collection) {
+    for(let j =0; j< collection.length; j++){
+        if(element === collection[j].name){
+            return collection[j];
+        }
+    }
+}
+function isSymbol(element){
+    let result = {};
+    if(element.includes('-')){
+        result.name = element.split('-')[0];
+        result.summary = parseInt(element.split('-')[1]);
+    } else if(element.includes(':')){
+        result.name = element.split(':')[0];
+        result.summary = parseInt(element.split(':')[1]);
+    } else if(element.includes('[')){
+        result.name = element.split('[')[0];
+        result.summary = parseInt(element.split('[')[1].split(']')[0]);
+    } else {
+        return null;
+    }
+    return result;
 }
