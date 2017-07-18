6c8b16297233d5589520cde693afdb27c52b414d
Aym-fuhong <18829287015@163.com>
2017-07-18 23:29:07 +0800

    finish section3-practice1

--- workspace/gpst-js-basic-collection-practice/main/section-2/practice-1.js
+++ workspace/gpst-js-basic-collection-practice/main/section-2/practice-1.js
@@ -3,7 +3,7 @@
 module.exports = function countSameElements(collection) {
   let result = [];
   for(let i =0 ; i < collection.length; i++) {
-    let obj = isExist(collection[i], result);
+    let obj = findOne(collection[i], result);
     if(obj){
         obj.count++;
     } else {
@@ -13,7 +13,7 @@ module.exports = function countSameElements(collection) {
   return result;
 };
 
-function isExist(element, collection) {
+function findOne(element, collection) {
   for(let j =0; j< collection.length; j++){
   if(element === collection[j].key){
     return collection[j];
