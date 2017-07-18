7b753285d4f508df76f2a7d6ed7e96a49fd020bd
Aym-fuhong <18829287015@163.com>
2017-07-18 15:06:37 +0800

    finish section2-practise2

--- workspace/gpst-js-basic-collection-practice/main/section-2/practice-2.js
+++ workspace/gpst-js-basic-collection-practice/main/section-2/practice-2.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+    let result = [];
+    for(let i =0 ; i < collection.length; i++) {
+        let obj = isExist(collection[i], result);
+        if(obj){
+            obj.count++;
+        } else if(collection[i].includes('-')) {
+          let combNum = collection[i].split('-');
+          result.push({key: combNum[0], count: parseInt(combNum[1])});
+        }else {
+            result.push({key: collection[i], count: 1});
+        }
+    }
+    return result;
+};
+
+function isExist(element, collection) {
+    for(let j =0; j< collection.length; j++){
+        if(element === collection[j].key){
+            return collection[j];
+        }
+    }
 }
