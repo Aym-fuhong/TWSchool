4fc608420e5d4ab75995ff2552a0ea831ef052c8
Aym-fuhong <18829287015@163.com>
2017-07-19 00:19:29 +0800

    finish section3-practice3

--- workspace/gpst-js-basic-collection-practice/main/section-3/practice-3.js
+++ workspace/gpst-js-basic-collection-practice/main/section-3/practice-3.js
@@ -1,5 +1,44 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let result = getObjOfArray(collectionA);
+    for(let i =0; i< result.length; i++){
+        if(isExist(result[i].key , objectB.value)){
+            if(result[i].count >= 3 ) {
+                result[i].count -= Math.floor(result[i].count / 3 );
+            }
+        }
+
+    }
+    return result;
+};
+
+function getObjOfArray(collection){
+  let result = [];
+  for(let i = 0; i< collection.length; i++){
+      if(findOne(collection[i], result)){
+        let obj = findOne(collection[i], result);
+        obj.count++;
+      } else {
+        result.push({"key": collection[i], "count": 1});
+      }
+  }
+return result;
+};
+
+function findOne(element, collection) {
+    for(let j =0; j< collection.length; j++){
+        if(element === collection[j].key){
+            return collection[j];
+        }
+    }
+};
+
+function isExist(element, collection){
+    for(let i = 0; i< collection.length;i++){
+        if(element === collection[i]) {
+            return true;
+        }
+    }
+    return false;
 }
