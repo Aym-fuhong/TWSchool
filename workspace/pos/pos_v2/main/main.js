1d8bceb4ae6e3521316f26b08b1bcf5cc0bfc463
Aym-fuhong <18829287015@163.com>
2017-07-20 20:48:58 +0800

    complete print receipt of module 4 and use 6 minutes

--- workspace/pos/pos_v2/main/main.js
+++ workspace/pos/pos_v2/main/main.js
@@ -17,28 +17,12 @@ function formatTags(collection) {
   return formattedTags;
 }
 
-function getCartItems(formattedTags, items) {
-  return formattedTags.map(item => {
-    if (findItem(item.barcode, items)) {
-      let element = findItem(item.barcode, items);
-      item.name = element.name;
-      item.unit = element.unit;
-      item.price = element.price;
-    }
-    return item;
-  });
-}
-
 function calculatePrice(cartItems) {
   let itemsPromotionType = getItemsPromotionType(cartItems, Promotions);
   let itemsPrice = getItemsPrice(itemsPromotionType);
   let receiptObj = getReceiptObj(itemsPrice);
   return receiptObj;
 
-}
-
-function getReceiptStr(receiptobj) {
-  let formattedCurrentDate = getCurrentDate();
 }
 
 function splitTags(collection) {
@@ -65,6 +49,19 @@ function getItemsCount(splitedTags) {
   return formattedTags;
 }
 
+
+function getCartItems(formattedTags, items) {
+  return formattedTags.map(item => {
+    if (findItem(item.barcode, items)) {
+      let element = findItem(item.barcode, items);
+      item.name = element.name;
+      item.unit = element.unit;
+      item.price = element.price;
+    }
+    return item;
+  });
+}
+
 function getItemsPromotionType(cartItems, promotion) {
   return cartItems.map(item => {
       promotion.forEach(element => {
@@ -80,7 +77,7 @@ function getItemsPromotionType(cartItems, promotion) {
 function getItemsPrice(itemsPromotionType) {
   return itemsPromotionType.map(item => {
     if (item.type) {
-      if(item.count >= 3){
+      if (item.count >= 3) {
         item.itemPrice = parseFloat(item.price * (item.count - 1));
       }
     } else {
@@ -91,13 +88,13 @@ function getItemsPrice(itemsPromotionType) {
 
 }
 
-function getReceiptObj(itemsPrice){
+function getReceiptObj(itemsPrice) {
   let receiptObj = {};
   let totalPrice = 0;
   let savedPrice = 0;
   itemsPrice.forEach(item => {
     totalPrice += item.itemPrice;
-    savedPrice += item.count*item.price - item.itemPrice;
+    savedPrice += item.count * item.price - item.itemPrice;
   });
   receiptObj.items = itemsPrice;
   receiptObj.totalPrice = parseFloat(totalPrice);
@@ -105,8 +102,32 @@ function getReceiptObj(itemsPrice){
   return receiptObj;
 }
 
+function getReceiptStr(receiptObj) {
+  let formattedCurrentDate = getCurrentDate();
+  let line = `***<没钱赚商店>收据***\n`;
+  line += formattedCurrentDate;
+  line += `----------------------\n`;
+  for (let i = 0; i < receiptObj.items.length; i++) {
+    let item = receiptObj.items[i];
+    line += `名称：${item.name}，数量：${item.count}${item.unit}，单价：${item.price.toFixed(2)}(元)，小计：${item.itemPrice.toFixed(2)}(元)\n`
+  }
+  line += `----------------------\n`;
+  line += `总计：${receiptObj.totalPrice.toFixed(2)}(元)
+节省：${receiptObj.savedPrice.toFixed(2)}(元)
+**********************`;
+  return line;
+}
 function getCurrentDate() {
-  return '';
+  const dateDigitToString = num => (num < 10 ? `0${num}` : num);
+  const currentDate = new Date(),
+    year = dateDigitToString(currentDate.getFullYear()),
+    month = dateDigitToString(currentDate.getMonth() + 1),
+    date = dateDigitToString(currentDate.getDate()),
+    hour = dateDigitToString(currentDate.getHours()),
+    minute = dateDigitToString(currentDate.getMinutes()),
+    second = dateDigitToString(currentDate.getSeconds()),
+    formattedDateString = `打印时间：${year}年${month}月${date}日 ${hour}:${minute}:${second}\n`;
+  return formattedDateString;
 }
 
 function isPromotionItem(element, collection) {
