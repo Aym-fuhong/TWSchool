9a0622c630a17b8ebfd4fff7b1e7a74d271c8053
Aym-fuhong <18829287015@163.com>
2017-07-20 17:31:00 +0800

    compelete thinking code framework

--- workspace/pos/pos_v2/main/main.js
+++ workspace/pos/pos_v2/main/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+let AllItems = Item.all();
+let Promotions = Promotion.all();
+
+function printReceipt(collection) {
+  let formattedTags = formatTags(collection);
+  let cartItems = getCartItems(formattedTags, AllItems);
+  let receiptObj = calculatePrice(cartItems);
+  let result = getReceiptStr(receiptObj, currentDate);
+  return result;
+}
+
+function formatTags(collection) {
+  let splitedTags = splitTags(collection);
+  let formattedTags = getItemsCount(splitedTags);
+  return formattedTags;
+}
+
+function calculatePrice(cartItems){
+  let itemsPromotionType = getItemsPromotionType(cartItems, Promotions);
+  let itemsPrice = getItemsPrice(itemsPromotionType);
+  let receiptObj = getReceiptObj(itemsPrice);
+  return receiptObj;
+
+}
+function getReceiptStr(receiptobj) {
+  let formattedCurrentDate = getCurrentDate();
+}
+
+function getCurrentDate(){
+  return '';
+}
+function getCartItems(collection, items) {
+
+}
+
+
+
+
