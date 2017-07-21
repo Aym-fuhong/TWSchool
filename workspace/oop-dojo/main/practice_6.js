f0496c33227734209d180fc7d68e280becd187b4
Aym-fuhong <18829287015@163.com>
2017-07-21 17:50:39 +0800

    prictice6 and test completed + 20minutes

---
+++ workspace/oop-dojo/main/practice_6.js
@@ -0,0 +1,32 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    introduce() {
+        return `My name is ${this.name}. I am ${this.age} years old.`;
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, class1) {
+        super(name, age);
+        this.class1 = class1;
+    }
+    introduce() {
+        return `${super.introduce()} I am a Student. I am at Class 2.`;
+
+    }
+}
+
+class Worker extends Person {
+    constructor(name, age) {
+        super(name, age);
+    }
+    introduce() {
+        return `${this.introduce()} I am a Worker. I have a job.`
+
+    }
+}
+
+module.exports = {Person, Student, Worker};
--- workspace/oop-dojo/spec/practice_5/main_spec.js
+++ workspace/oop-dojo/spec/practice_5/main_spec.js
@@ -3,15 +3,12 @@ const {Person, Student, Worker} = require('../../main/practice_5');
 describe("O5门", function(){
     it("覆盖同时复用父类的方法", function(){
         const person = new Person('Tom', 21);
-        console.log(person.basic_introduce(),'123456789--------');
-
         const result1 = `My name is Tom. I am 21 years old.`;
         expect(person.introduce()).toEqual(result1);
     });
 
     it("覆盖同时复用父类的方法", function(){
         const student = new Student('Tom', 21, 2);
-        console.log(student.basic_introduce(),'55555555--------');
         const result2 = `My name is Tom. I am 21 years old. I am a Student. I am at Class 2.`;
         expect(student.introduce()).toEqual(result2);
     });
