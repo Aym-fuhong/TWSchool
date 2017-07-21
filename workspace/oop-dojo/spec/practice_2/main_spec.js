7f39b93beea761fd1e90c2f125064639b36a3ebe
Aym-fuhong <18829287015@163.com>
2017-07-21 15:47:22 +0800

    practice2 and test completed + 3 minutes

--- workspace/oop-dojo/spec/practice_2/main_spec.js
+++ workspace/oop-dojo/spec/practice_2/main_spec.js
@@ -1,7 +1,9 @@
 "use strict";
-
+const Person = require('../../main/practice_2');
 describe("OO入门", function(){
     it("写一个类-2", function(){
-
+        const person = new Person('Tom', 21);
+        let result = 'My name is Tom. I am 21 years old. ';
+        expect(person.introduce()).toEqual(result);
     });
 });
