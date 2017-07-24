4217328362b2f00b6d1c5a6779544eafab1a6484
Aym-fuhong <18829287015@163.com>
2017-07-24 23:56:43 +0800

    finish practice_4 and test , predict 10minutes and actual 15 minutes

---
+++ workspace/oop-dojo-java/src/main/java/practice_4/Person.java
@@ -0,0 +1,20 @@
+package practice_4;
+
+/**
+ * Created by Hong on 24/07/2017.
+ */
+public class Person {
+
+    private int age;
+    private String name;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        String result = "My name is " + this.name + ". I am " + this.age + " years old.";
+        return result;
+    }
+}
