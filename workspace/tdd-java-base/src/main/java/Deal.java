d96bce09f15ee181657f5ce2521f11cd33b07653
Aym-fuhong <18829287015@163.com>
2017-07-25 16:11:44 +0800

    should_return_string_list_when_input_string_include_one_space test and  function finished predict 5 minutes and actual 10 minutes

---
+++ workspace/tdd-java-base/src/main/java/Deal.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Hong on 25/07/2017.
+ */
+public class Deal {
+
+
+    public String[] formatWords(String words) {
+        String[] wordList = words.split(" ");
+        return wordList;
+    }
+}
---
+++ workspace/tdd-java-base/src/test/java/DealTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DealTest {
+
+    @Test
+    public void should_return_string_list_when_input_string_include_one_space() throws Exception {
+        String words = "the day is sunny";
+        String[] result = {"the", "day", "is", "sunny"};
+        Deal deal = new Deal();
+        assertThat(deal.formatWords(words), is(result));
+    }
+}
