f71ef1553f465602a8e5bdd1a79bbc14cfbcf73b
Aym-fuhong <18829287015@163.com>
2017-08-02 23:59:23 +0800

    deleted no use code

--- workspace/uml-testing-display/js/uml-paper.js
+++ workspace/uml-testing-display/js/uml-paper.js
@@ -1,10 +1,14 @@
 $(function () {
     $(":submit[id=tijiao]").click(function (check) {
+        let email = $(":text[id=email]").val();
         let card = $(":text[id=card]").val();
-        if (card == "") {
+        if (card === "") {
             alert("文本框输入为空！");
             $(":text[id=card]").focus();
-        } else if (card.match("/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/") === false) {
+        } else if(email === ""){
+            alert("文本框输入为空！");
+            $(":text[id=email]").focus();
+        }else if (card.match("/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/") === false) {
             alert("格式错误！");
             $(":text[id=card]").focus();
         } else {
