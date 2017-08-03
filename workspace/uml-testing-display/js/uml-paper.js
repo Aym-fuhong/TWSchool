b8ed567ffb96e3d5bd72b126c0ae65f7d8652719
Aym-fuhong <18829287015@163.com>
2017-08-03 14:09:44 +0800

    update form submit way

--- workspace/uml-testing-display/js/uml-paper.js
+++ workspace/uml-testing-display/js/uml-paper.js
@@ -1,31 +1,61 @@
 $(function () {
-    $(":submit[id=tijiao]").click(function (check) {
-        let email = $(":text[id=email]").val();
-        let card = $(":text[id=card]").val();
-        if (card === "") {
-            alert("文本框输入为空！");
-            $(":text[id=card]").focus();
-        } else if(email === ""){
-            alert("文本框输入为空！");
-            $(":text[id=email]").focus();
-        }else if (card.match("/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/") === false) {
-            alert("格式错误！");
-            $(":text[id=card]").focus();
-        } else {
+    // $("form").submit(function () {
+    //     let dataArray = $('form').serializeArray();
+    //     // let newDataArray = [];
+    //     // $.forEach(dataArray, function (index, element) {
+    //     //     if (findOne(element.name, newDataArray)) {
+    //     //         let item = findOne(element.name, newDataArray);
+    //     //         item.value += element.value
+    //     //     } else {
+    //     //         newDataArray.add(element);
+    //     //     }
+    //     // });
+    //
+    //     localStorage.setItem("dataArray", JSON.stringify(dataArray));
+    //     location.href = 'display.html';
+    //     // }
+    //     event.preventDefault();
+    //     // return false;
+    // });
+
+    $("#form-paper").validate({
+        rules: {
+            class: "required",
+            student_number: "required",
+            name: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            ID_Card: {
+                required: true,
+                minlength: 5
+
+            }
+        },
+        messages: {
+            class: "请输入班级",
+            student_number: "学号不能为空",
+            name: "姓名不能为空",
+            email: {
+                required: "邮箱不能为空",
+                email: "邮箱格式错误"
+            },
+            ID_Card: {
+                required: "身份证号不能为空",
+                minlength: "身份证号长度不能小于5"
+            }
+        },
+        submitHandler:function(form){
+            form.event.preventDefault();
             let dataArray = $('form').serializeArray();
-            // let newDataArray = [];
-            // $.forEach(dataArray, function (index, element) {
-            //     if (findOne(element.name, newDataArray)) {
-            //         let item = findOne(element.name, newDataArray);
-            //         item.value += element.value
-            //     } else {
-            //         newDataArray.add(element);
-            //     }
-            // });
             localStorage.setItem("dataArray", JSON.stringify(dataArray));
-            location.href = 'display.html';
+                location.href = 'display.html';
         }
-        return false;
+        // showErrors: function (errorMap, errorList) {
+        //     $("#error").html("Your form contains " + this.numberOfInvalids() + " errors,see details below.");
+        //     this.defaultShowErrors();
+        // },
     });
 });
 function findOne(element, collection) {
