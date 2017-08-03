75cf6565c52cfaa702dd4c10bb9e611519054a79
Aym-fuhong <18829287015@163.com>
2017-08-04 01:02:46 +0800

    finished show studentlist and search student info [predict an hour actual 30 minutes]

--- workspace/student-grade-web/js/search_student.js
+++ workspace/student-grade-web/js/search_student.js
@@ -1,3 +1,34 @@
-/**
- * Created by Hong on 03/08/2017.
- */
+const studentsInfo = JSON.parse(localStorage.getItem("studentsInfo"));
+
+function search() {
+    let studentId = $("#id").val();
+    $("#tbody").remove();
+    studentsInfo.map(item => {
+        let tr = "<tr>";
+        if (item[5].value === parseInt(studentId)) {
+            tr += "<td>" + item[5].value + "</td>";
+            tr += "<td>" + item[0].value + "</td>";
+            tr += "<td>" + item[1].value + "</td>";
+            tr += "<td>" + item[2].value + "</td>";
+            tr += "<td>" + item[3].value + "</td>";
+            tr += "<td>" + item[4].value + "</td>";
+        }
+        tr += "</tr>";
+        $("#table").append(tr);
+    })
+}
+
+$(function () {
+    studentsInfo.map(item => {
+        let tr = "<tr>";
+        tr += "<td>" + item[5].value + "</td>";
+        tr += "<td>" + item[0].value + "</td>";
+        tr += "<td>" + item[1].value + "</td>";
+        tr += "<td>" + item[2].value + "</td>";
+        tr += "<td>" + item[3].value + "</td>";
+        tr += "<td>" + item[4].value + "</td>";
+        tr += "</tr>";
+        $("#table").append(tr);
+    })
+
+});
