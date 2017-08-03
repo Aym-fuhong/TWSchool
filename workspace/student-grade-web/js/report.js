14167bb2b30f0abc031732dd35b6d16534b2f1ac
Aym-fuhong <18829287015@163.com>
2017-08-04 01:39:18 +0800

    finished report page style and show info [predict 40 minutes actual 20 minutes]

--- workspace/student-grade-web/js/report.js
+++ workspace/student-grade-web/js/report.js
@@ -1,3 +1,26 @@
-/**
- * Created by Hong on 03/08/2017.
- */
+let reportObj = {
+    studentInfo: [
+        {name: "aym", language: 89, math: 99, English: 100, program: 100},
+        {name: "jack", language: 89, math: 99, English: 100, program: 100},
+        {name: "rose", language: 89, math: 99, English: 100, program: 100},
+    ],
+    classOverage: 123,
+    classMiddle: 234
+};
+localStorage.setItem("reportObj", JSON.stringify(reportObj));
+
+$(function () {
+    const reportObj = JSON.parse(localStorage.getItem("reportObj"));
+    reportObj.studentInfo.map(item => {
+        let tr = "<tr>";
+        tr += "<td>" + item.name + "</td>";
+        tr += "<td>" + item.language + "</td>";
+        tr += "<td>" + item.math + "</td>";
+        tr += "<td>" + item.English + "</td>";
+        tr += "<td>" + item.program + "</td>";
+        tr += "</tr>";
+        $("#table").append(tr);
+    });
+    $("#average").val(reportObj.classOverage);
+    $("#middle").val(reportObj.classMiddle);
+});
