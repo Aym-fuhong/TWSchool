387659a969b751491b859012ab76a78c1e261e5b
Aym-fuhong <18829287015@163.com>
2017-08-04 16:07:20 +0800

    finished get employees [predict 5 minutes actual 3 mnutes]

--- workspace/spring-boot-base/src/main/java/com/student/conroller/HelloController.java
+++ workspace/spring-boot-base/src/main/java/com/student/conroller/HelloController.java
@@ -5,11 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 @RestController
 public class HelloController {
-    private HashMap<Integer, Employee> employeesMap = new HashMap<>();
+    private ArrayList<Employee> employeeList = new ArrayList<>();
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public ResponseEntity<String> string() {
@@ -19,13 +19,17 @@ public class HelloController {
     @RequestMapping(value = "/employee", method = RequestMethod.POST)
     public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
         employee.setId(1);
-        employeesMap.put(1, employee);
+        employeeList.add(employee);
         return new ResponseEntity<Employee>(employee, HttpStatus.CREATED);
     }
     @RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
     public Employee getEmployeeById(@PathVariable("id")int id){
-        Employee employee = employeesMap.get(id);
+        Employee employee = employeeList.get(id);
         return employee;
+    }
+    @RequestMapping(value = "/employees", method = RequestMethod.GET)
+    public ArrayList<Employee> getEmployees(){
+        return employeeList;
     }
 
 }
