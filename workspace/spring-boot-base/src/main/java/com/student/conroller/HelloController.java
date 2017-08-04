a6e99b90a60ba71f0d210b4cc0f2f974ae7a2d25
Aym-fuhong <18829287015@163.com>
2017-08-04 16:03:03 +0800

    finished add a employee and find a employee [predict 10 minutes and 15 minutes]

--- workspace/spring-boot-base/src/main/java/com/student/conroller/HelloController.java
+++ workspace/spring-boot-base/src/main/java/com/student/conroller/HelloController.java
@@ -1,17 +1,31 @@
 package com.student.conroller;
 
+import com.student.model.Employee;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 public class HelloController {
+    private HashMap<Integer, Employee> employeesMap = new HashMap<>();
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public ResponseEntity<String> string() {
         return new ResponseEntity<String>("hello", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/employee", method = RequestMethod.POST)
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        employee.setId(1);
+        employeesMap.put(1, employee);
+        return new ResponseEntity<Employee>(employee, HttpStatus.CREATED);
+    }
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
+    public Employee getEmployeeById(@PathVariable("id")int id){
+        Employee employee = employeesMap.get(id);
+        return employee;
     }
 
 }
---
+++ workspace/spring-boot-base/src/main/java/com/student/model/Employee.java
@@ -0,0 +1,31 @@
+package com.student.model;
+
+public class Employee {
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
