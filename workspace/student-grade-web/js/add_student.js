7fcd71e26308a313f3ae3dadd9fb39765e33594e
Aym-fuhong <18829287015@163.com>
2017-08-08 20:57:53 +0800

    finished get studentlist request [predict 15 minutes actual 25 minutes]

--- workspace/student-grade-web/js/add_student.js
+++ workspace/student-grade-web/js/add_student.js
@@ -1,4 +1,3 @@
-
 $(function () {
     $("#form").validate({
         rules: {
--- workspace/student-grade-web/js/search_student.js
+++ workspace/student-grade-web/js/search_student.js
@@ -1,17 +1,15 @@
-const studentsInfo = JSON.parse(localStorage.getItem("studentsInfo"));
-
 function search() {
     let studentId = $("#id").val();
     $("#tbody").remove();
     studentsInfo.map(item => {
         let tr = "<tr>";
-        if (item[5].value === parseInt(studentId)) {
-            tr += "<td>" + item[5].value + "</td>";
-            tr += "<td>" + item[0].value + "</td>";
-            tr += "<td>" + item[1].value + "</td>";
-            tr += "<td>" + item[2].value + "</td>";
-            tr += "<td>" + item[3].value + "</td>";
-            tr += "<td>" + item[4].value + "</td>";
+        if (item.id === parseInt(studentId)) {
+            tr += "<td>" + item.id + "</td>";
+            tr += "<td>" + item.name + "</td>";
+            tr += "<td>" + item.email + "</td>";
+            tr += "<td>" + item.id_Card + "</td>";
+            tr += "<td>" + item.phone + "</td>";
+            tr += "<td>" + item.native_place + "</td>";
         }
         tr += "</tr>";
         $("#table").append(tr);
@@ -19,16 +17,27 @@ function search() {
 }
 
 $(function () {
-    studentsInfo.map(item => {
-        let tr = "<tr>";
-        tr += "<td>" + item[5].value + "</td>";
-        tr += "<td>" + item[0].value + "</td>";
-        tr += "<td>" + item[1].value + "</td>";
-        tr += "<td>" + item[2].value + "</td>";
-        tr += "<td>" + item[3].value + "</td>";
-        tr += "<td>" + item[4].value + "</td>";
-        tr += "</tr>";
-        $("#table").append(tr);
-    })
+    $.ajax({
+        type: 'GET',
+        url: "http://localhost:8080/students",
+        dataType: "json",
+        contentType: "application/json;charset=utf-8",
+        success: function (data) {
+            const studentInfo = data;
+            console.log(studentInfo);
+            studentInfo.map(item => {
+                let tr = "<tr>";
+                tr += "<td>" + item.id + "</td>";
+                tr += "<td>" + item.name + "</td>";
+                tr += "<td>" + item.email + "</td>";
+                tr += "<td>" + item.id_Card + "</td>";
+                tr += "<td>" + item.phone + "</td>";
+                tr += "<td>" + item.native_place + "</td>";
+                tr += "</tr>";
+                $("#table").append(tr);
+            })
+        }
+    });
+
 
 });
