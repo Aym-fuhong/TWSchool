1ccf90297936b824532bdf1a4e8ae33cbf1e94d1
Aym-fuhong <18829287015@163.com>
2017-08-08 17:25:38 +0800

    finished add student api and web connect [predict 30 minutes actual 45 minutes]

--- workspace/student-grade-web/js/add_student.js
+++ workspace/student-grade-web/js/add_student.js
@@ -10,38 +10,45 @@ $(function () {
     $("#form").validate({
         rules: {
             user_name: "required",
-            province: "required",
-            city: "required",
-            phone: "required",
-            native_place: "required",
-            email: {
-                required: true,
-                email: true
-            },
-            ID_Card: {
-                required: true,
-                minlength: 5
-
-            }
+            math: "required",
+            language: "required",
+            english: "required",
+            program: "required"
         },
         messages: {
             user_name: "请输入姓名",
-            phone: "手机号不能为空",
-            native_place: "籍贯不能为空",
-            email: {
-                required: "邮箱不能为空",
-                email: "邮箱格式错误"
+            math: {
+                required: "数学成绩不能为空",
             },
-            ID_Card: {
-                required: "身份证号不能为空",
-                minlength: "身份证号长度不能小于5"
-            }
+            language: {
+                required: "语文成绩不能为空",
+            },
+            english: {
+                required: "英语成绩不能为空",
+            },
+            program: {
+                required: "编程成绩不能为空",
+            },
+
         },
         submitHandler: function (form) {
             event.preventDefault();
             let dataArray = $('#form').serializeArray();
-            addStudent(dataArray);
-            alert("学生添加成功！");
+            let studentObj = {};
+            dataArray.map(item => {
+                studentObj[item.name] = item.value;
+            });
+            $.ajax({
+                type: 'POST',
+                url: "http://localhost:8080/students",
+                data: JSON.stringify(studentObj),
+                dataType: "json",
+                contentType: "application/json;charset=utf-8",
+                success: function () {
+                    alert("学生添加成功！");
+                }
+            });
+
             $("#form")[0].reset();
 
         }
