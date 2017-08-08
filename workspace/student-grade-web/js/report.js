6e7ad61bcab8d3e0dd01a1cd8ee9039443af699a
Aym-fuhong <18829287015@163.com>
2017-08-08 17:58:30 +0800

    finished class report [predict 20 minutes actual 30 minutes]

--- workspace/student-grade-web/js/report.js
+++ workspace/student-grade-web/js/report.js
@@ -10,17 +10,26 @@ let reportObj = {
 localStorage.setItem("reportObj", JSON.stringify(reportObj));
 
 $(function () {
-    const reportObj = JSON.parse(localStorage.getItem("reportObj"));
-    reportObj.studentInfo.map(item => {
-        let tr = "<tr>";
-        tr += "<td>" + item.name + "</td>";
-        tr += "<td>" + item.language + "</td>";
-        tr += "<td>" + item.math + "</td>";
-        tr += "<td>" + item.English + "</td>";
-        tr += "<td>" + item.program + "</td>";
-        tr += "</tr>";
-        $("#table").append(tr);
+    $.ajax({
+        type: 'GET',
+        url: "http://localhost:8080/reports",
+        dataType: "json",
+        contentType: "application/json;charset=utf-8",
+        success: function (data) {
+           const reportObj = data;
+           console.log(reportObj);
+            reportObj.studentList.map(item => {
+                let tr = "<tr>";
+                tr += "<td>" + item.name + "</td>";
+                tr += "<td>" + item.language + "</td>";
+                tr += "<td>" + item.math + "</td>";
+                tr += "<td>" + item.english + "</td>";
+                tr += "<td>" + item.program + "</td>";
+                tr += "</tr>";
+                $("#table").append(tr);
+            });
+            $("#average").val(reportObj.averageOfClass);
+            $("#middle").val(reportObj.medianOfClass);
+        }
     });
-    $("#average").val(reportObj.classOverage);
-    $("#middle").val(reportObj.classMiddle);
 });
